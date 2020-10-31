c894f805fd533e382305d2f6124b4193ea39e3de
meanstack4@gmail.com <meanstack4@gmail.com>
2020-10-31 10:52:37 +0530

    projecr setup

---
+++ app/src/main/java/com/e/hardwarewalaseller/MainActivity.java
@@ -0,0 +1,16 @@
+package com.e.hardwarewalaseller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
