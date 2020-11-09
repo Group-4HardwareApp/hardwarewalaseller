323050a604a70ac70a775842f2a6f2ae69afcdd4
damascus8 <siddharthshrivastav30@gmail.com>
2020-11-09 12:18:54 +0530

    AddStoreUI & Code

--- app/src/main/java/com/e/hardwarewalaseller/MainActivity.java
+++ app/src/main/java/com/e/hardwarewalaseller/MainActivity.java
@@ -7,10 +7,10 @@ import android.os.Bundle;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
---
+++ app/src/main/java/com/e/hardwarewalaseller/apis/ServerAddress.java
@@ -0,0 +1,7 @@
+package com.e.hardwarewalaseller.apis;
+
+public interface ServerAddress {
+    public static String BASE_URL="http://192.168.43.47:8080/";
+
+//    127.0.0.1
+}
